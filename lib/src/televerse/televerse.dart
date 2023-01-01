026e036d1dc1a5fdc3b7e71157f62268417c280e
Sreelal TS <heysreelal@gmail.com>
2023-01-01 18:01:28 +0530

    Passport errors are set 🧑🏻‍💻

--- lib/src/televerse/televerse.dart
+++ lib/src/televerse/televerse.dart
@@ -2964,4 +2964,23 @@ class Televerse extends Event {
 
     return response;
   }
+
+  /// Informs a user that some of the Telegram Passport elements they provided contains errors. The user will not be able to re-submit their Passport to you until the errors are fixed (the contents of the field for which you returned the error must change). Returns True on success.
+  /// Use this if the data submitted by the user doesn't satisfy the standards your service requires for any reason. For example, if a birthday date seems invalid, a submitted document is blurry, a scan shows evidence of tampering, etc. Supply some details in the error message to make sure the user knows how to correct the issues.
+  Future<bool> setPassportDataErrors(
+    int userId,
+    List<PassportElementError> errors,
+  ) async {
+    Map<String, dynamic> params = {
+      "user_id": userId,
+      "errors": errors.map((e) => e.toJson()).toList(),
+    };
+
+    bool response = await HttpClient.postURI(
+      _buildUri("setPassportDataErrors"),
+      params,
+    );
+
+    return response;
+  }
 }
