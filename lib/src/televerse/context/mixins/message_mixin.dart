8469b3045338aff0f405e4010763a11246e5a01e
Sreelal TS <heysreelal@gmail.com>
2023-01-07 13:23:13 +0530

    Chefs are good when lords with code! 🍟

--- lib/src/televerse/context/mixins/message_mixin.dart
+++ lib/src/televerse/context/mixins/message_mixin.dart
@@ -302,4 +302,244 @@ mixin MessageMixin on Context {
       replyMarkup: replyMarkup,
     );
   }
+
+  /// Reply with a Venue to the user.
+  ///
+  /// Provide a [latitude], a [longitude], a [title] and an [address] to send a venue.
+  Future<Message> replyWithVenue(
+    double latitude,
+    double longitude,
+    String title,
+    String address, {
+    int? messageThreadId,
+    String? foursquareId,
+    String? foursquareType,
+    String? googlePlaceId,
+    String? googlePlaceType,
+    bool? disableNotification,
+    bool? protectContent,
+    int? replyToMessageId,
+    bool? allowSendingWithoutReply,
+    ReplyMarkup? replyMarkup,
+  }) async {
+    return await api.sendVenue(
+      id,
+      latitude,
+      longitude,
+      title,
+      address,
+      messageThreadId: messageThreadId,
+      foursquareId: foursquareId,
+      foursquareType: foursquareType,
+      googlePlaceId: googlePlaceId,
+      googlePlaceType: googlePlaceType,
+      disableNotification: disableNotification,
+      protectContent: protectContent,
+      replyToMessageId: replyToMessageId,
+      allowSendingWithoutReply: allowSendingWithoutReply,
+      replyMarkup: replyMarkup,
+    );
+  }
+
+  /// Reply with a Contact to the user.
+  ///
+  /// Provide a [phoneNumber], a [firstName] and an [lastName] to send a contact.
+  Future<Message> replyWithContact(
+    String phoneNumber,
+    String firstName, {
+    int? messageThreadId,
+    String? lastName,
+    String? vcard,
+    bool? disableNotification,
+    bool? protectContent,
+    int? replyToMessageId,
+    bool? allowSendingWithoutReply,
+    ReplyMarkup? replyMarkup,
+  }) async {
+    return await api.sendContact(
+      id,
+      phoneNumber,
+      firstName,
+      lastName: lastName,
+      messageThreadId: messageThreadId,
+      vcard: vcard,
+      disableNotification: disableNotification,
+      protectContent: protectContent,
+      replyToMessageId: replyToMessageId,
+      allowSendingWithoutReply: allowSendingWithoutReply,
+      replyMarkup: replyMarkup,
+    );
+  }
+
+  /// Reply with a Poll to the user.
+  ///
+  /// Provide a [question], a list of [options] and a [type] to send a poll.
+  Future<Message> replyWithPoll(
+    String question,
+    List<String> options, {
+    int? messageThreadId,
+    bool? isAnonymous,
+    PollType type = PollType.regular,
+    bool? allowsMultipleAnswers,
+    int? correctOptionId,
+    String? explanation,
+    ParseMode? explanationParseMode,
+    List<MessageEntity>? explanationEntities,
+    int? openPeriod,
+    DateTime? closeDate,
+    bool? isClosed,
+    bool? disableNotification,
+    bool? protectContent,
+    int? replyToMessageId,
+    bool? allowSendingWithoutReply,
+    ReplyMarkup? replyMarkup,
+  }) async {
+    return await api.sendPoll(
+      id,
+      question,
+      options,
+      messageThreadId: messageThreadId,
+      isAnonymous: isAnonymous,
+      type: type,
+      allowsMultipleAnswers: allowsMultipleAnswers,
+      correctOptionId: correctOptionId,
+      explanation: explanation,
+      explanationParseMode: explanationParseMode,
+      explanationEntities: explanationEntities,
+      openPeriod: openPeriod,
+      closeDate: closeDate,
+      isClosed: isClosed,
+      disableNotification: disableNotification,
+      protectContent: protectContent,
+      replyToMessageId: replyToMessageId,
+      allowSendingWithoutReply: allowSendingWithoutReply,
+      replyMarkup: replyMarkup,
+    );
+  }
+
+  /// Reply with a Dice to the user.
+  ///
+  /// Provide a [emoji] to send a dice.
+  Future<Message> replyWithDice({
+    DiceEmoji emoji = DiceEmoji.dice,
+    int? messageThreadId,
+    bool? disableNotification,
+    bool? protectContent,
+    int? replyToMessageId,
+    bool? allowSendingWithoutReply,
+    ReplyMarkup? replyMarkup,
+  }) async {
+    return await api.sendDice(
+      id,
+      emoji: emoji,
+      messageThreadId: messageThreadId,
+      disableNotification: disableNotification,
+      protectContent: protectContent,
+      replyToMessageId: replyToMessageId,
+      allowSendingWithoutReply: allowSendingWithoutReply,
+      replyMarkup: replyMarkup,
+    );
+  }
+
+  /// Reply with a Chat Action to the user.
+  ///
+  /// Provide a [action] to send a chat action.
+  Future<bool> replyWithChatAction(
+    ChatAction action, {
+    int? messageThreadId,
+  }) async {
+    return await api.sendChatAction(
+      id,
+      action,
+      messageThreadId: messageThreadId,
+    );
+  }
+
+  /// Reply with a Game to the user.
+  ///
+  /// Provide a [shortName] to send a game.
+  Future<Message> replyWithGame(
+    String shortName, {
+    int? messageThreadId,
+    bool? disableNotification,
+    bool? protectContent,
+    int? replyToMessageId,
+    bool? allowSendingWithoutReply,
+    ReplyMarkup? replyMarkup,
+  }) async {
+    return await api.sendGame(
+      id,
+      shortName,
+      messageThreadId: messageThreadId,
+      disableNotification: disableNotification,
+      protectContent: protectContent,
+      replyToMessageId: replyToMessageId,
+      allowSendingWithoutReply: allowSendingWithoutReply,
+      replyMarkup: replyMarkup,
+    );
+  }
+
+  /// Reply with an Animation to the user.
+  ///
+  /// Provide an [animation] to send an animation.
+  Future<Message> replyWithAnimation(
+    InputFile animation, {
+    int? messageThreadId,
+    int? duration,
+    int? width,
+    int? height,
+    InputFile? thumb,
+    String? caption,
+    ParseMode? parseMode,
+    List<MessageEntity>? captionEntities,
+    bool? hasSpoiler,
+    bool? disableNotification,
+    bool? protectContent,
+    int? replyToMessageId,
+    bool? allowSendingWithoutReply,
+    ReplyMarkup? replyMarkup,
+  }) async {
+    return await api.sendAnimation(
+      id,
+      animation,
+      messageThreadId: messageThreadId,
+      duration: duration,
+      width: width,
+      height: height,
+      thumb: thumb,
+      caption: caption,
+      parseMode: parseMode,
+      captionEntities: captionEntities,
+      hasSpoiler: hasSpoiler,
+      disableNotification: disableNotification,
+      protectContent: protectContent,
+      replyToMessageId: replyToMessageId,
+      allowSendingWithoutReply: allowSendingWithoutReply,
+      replyMarkup: replyMarkup,
+    );
+  }
+
+  /// Reply with a Sticker to the user.
+  ///
+  /// Provide a [sticker] to send a sticker.
+  Future<Message> replyWithSticker(
+    InputFile sticker, {
+    String? messageThreadId,
+    bool? disableNotification,
+    bool? protectContent,
+    int? replyToMessageId,
+    bool? allowSendingWithoutReply,
+    InlineKeyboardMarkup? replyMarkup,
+  }) async {
+    return await api.sendSticker(
+      id,
+      sticker,
+      messageThreadId: messageThreadId,
+      disableNotification: disableNotification,
+      protectContent: protectContent,
+      replyToMessageId: replyToMessageId,
+      allowSendingWithoutReply: allowSendingWithoutReply,
+      replyMarkup: replyMarkup,
+    );
+  }
 }
