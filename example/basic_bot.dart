b0198cc953086e6926eda3b30a5fa226ed565dfb
Sreelal TS <heysreelal@gmail.com>
2023-01-07 23:45:58 +0530

    New Example 😻

---
+++ example/basic_bot.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+import 'package:televerse/televerse.dart';
+
+void main(List<String> args) {
+  final String token = Platform.environment["BOT_TOKEN"]!;
+
+  // Create a bot instance
+  Bot bot = Bot(token);
+
+  bot.command("start", (ctx) {
+    final String name = ctx.message.from!.firstName;
+    ctx.reply("Hello $name!");
+  });
+
+  bot.on(TeleverseEvent.text, (ctx) {
+    ctx as MessageContext;
+    final int letterCount = ctx.message.text!.length;
+    ctx.reply("Your message has $letterCount letters.");
+  });
+
+  bot.start();
+}
