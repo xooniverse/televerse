b98bc3a1efe5a0a94fa00e998501d9500e03feb0
Sreelal TS <heysreelal@gmail.com>
2023-02-25 15:18:28 +0530

    Entity and entities matcher 🏃🏻‍♀️

--- example/televerse_example.dart
+++ example/televerse_example.dart
@@ -7,27 +7,8 @@ void main() async {
 
   Bot bot = Bot(token, fetcher: LongPolling());
 
-  // Note: You can use the [onMessage] getter to listen to all messages
-  //     or use the [onCommand] getter to listen to commands
-  bot.onMessage.listen((MessageContext ctx) async {
-    // Collect the ChatID from the context
-    // This one is actually from v1.0.0, starting from v1.1.0 you can use the [ctx.id] getter
-    final chatId = ChatID(ctx.chat.id);
-
-    // Send a message to the chat
-    await ctx.reply("Hello World!");
-
-    // And with the [api] getter you can access the Telegram API
-    await ctx.api.sendMessage(chatId, "Hello World!");
-
-    // Looking for a way to send a photo?
-    // You can use the [sendPhoto] method
-    final photo = InputFile.fromUrl("https://i.imgur.com/1Z1Z1Z1.jpg");
-    await ctx.api.sendPhoto(ctx.id, photo);
-  });
-
   // Listen to commands
-  bot.command("bye", (ctx) => ctx.reply("Bye!"));
+  bot.command("hello", (ctx) => ctx.reply("Hello!"));
 
   // Add advanced filters to particularly listen to messages
   bot.filter((ctx) => false, (ctx) {
@@ -36,7 +17,10 @@ void main() async {
 
   // Or you can do this:
   bool myAdvancedFilter(MessageContext ctx) {
-    return (ctx.message.photo?.last.fileSize ?? 0) > 1000000;
+    return ctx.message.photo?.any((photo) {
+          return photo.fileSize! > 1 * 1000 * 1024;
+        }) ??
+        false;
   }
 
   bot.filter(myAdvancedFilter, (ctx) {
@@ -48,7 +32,7 @@ void main() async {
   // The [bot.hears] method allows you to listen to messages that match a regular expression.
   // You can use the `MessageContext.matches` getter to access the matches of the regular expression.
   bot.hears(RegExp(r'Hello, (.*)!'), (ctx) {
-    ctx.reply('Hello, ${ctx.matches![1]}!');
+    ctx.reply('${ctx.matches![1]} must be a doing great!');
   });
 
   // With the [Bot.on] method you can listen for particular updates and you can
@@ -64,4 +48,21 @@ void main() async {
   bot.start((ctx) => ctx.reply("Hello!"));
   bot.settings((ctx) => ctx.reply("Settings"));
   bot.help((ctx) => ctx.reply("Help"));
+
+  // So with the [Bot.on] method you can listen for particular updates. Yeah, that indeed means
+  // that you can listen for all commands simply by listening for the [TeleverseEvent.command] event.
+  bot.on(TeleverseEvent.command, (ctx) {
+    // ctx will be of type [MessageContext]
+    ctx as MessageContext;
+    ctx.reply("This will be executed for every command");
+
+    // Do your logic here
+    if (ctx.message.text == "televerse") {
+      ctx.reply("Much love from Televerse! ❤️");
+    }
+  });
+
+  bot.entity(MessageEntityType.mention, (ctx) {
+    ctx.reply("This will be executed for every mention");
+  });
 }
