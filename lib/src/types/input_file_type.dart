c8ae7f8c29454d136241641c4cf018b980171a94
Sreelal TS <heysreelal@gmail.com>
2023-04-30 13:01:16 +0530

    More docs & minor refactors ✨

--- lib/src/types/input_file_type.dart
+++ lib/src/types/input_file_type.dart
@@ -2,9 +2,9 @@ part of types;
 
 /// This object represents the type of a file to send.
 /// Currently, the following 3 types are supported:
-/// - [InputFileType.fileId]: For a file sent as a file_id
-/// - [InputFileType.file]: For a file sent as a file
-/// - [InputFileType.url]: For a file sent as a url
+/// - [InputFileType.fileId] : For a file sent as a file_id
+/// - [InputFileType.file] : For a file sent as a file
+/// - [InputFileType.url] : For a file sent as a url
 ///
 /// Check out the [InputFile] class for more information.
 enum InputFileType {
