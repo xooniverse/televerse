e3f1f854181d6c22e90563a0cf3348630690c150
Sreelal TS <heysreelal@gmail.com>
2023-05-08 14:09:11 +0530

    Deeplink methods 👽

--- lib/src/televerse/links/links.dart
+++ lib/src/televerse/links/links.dart
@@ -36,4 +36,114 @@ abstract class Deeplink {
 
     return Uri.encodeFull(parameter);
   }
+
+  /// Creates a [BotLink] with the given [username] and [startParameter].
+  static BotLink botLink(
+    String username, {
+    String? startParameter,
+  }) {
+    return BotLink(
+      username,
+      startParameter: startParameter,
+    );
+  }
+
+  /// Creates a [GroupBotLink] with the given [username], [startParameter] and [admin].
+  static GroupBotLink groupBotLink(
+    String username, {
+    String? startParameter,
+    BotAdminRights? admin,
+  }) {
+    return GroupBotLink(
+      username,
+      parameter: startParameter,
+      admin: admin,
+    );
+  }
+
+  /// Creates a [ChannelBotLink] with the given [username] and [admin].
+  static ChannelBotLink channelBotLink(
+    String username, {
+    required BotAdminRights admin,
+  }) {
+    return ChannelBotLink(
+      username,
+      admin: admin,
+    );
+  }
+
+  /// Creates a [GameLink] with the given [username] and [shortName].
+  static GameLink gameLink(
+    String username,
+    String shortName,
+  ) {
+    return GameLink(
+      username,
+      shortName,
+    );
+  }
+
+  /// Creates a [PhoneNumberLink] with the given [phoneNumber].
+  ///
+  /// The phone number should be in the format `+<country code><phone number>`.
+  static PhoneNumberLink phoneNumberLink(
+    String phoneNumber,
+  ) {
+    return PhoneNumberLink(
+      phoneNumber,
+    );
+  }
+
+  /// Creates a [UsernameLink] with the given [username].
+  static UsernameLink usernameLink(
+    String username,
+  ) {
+    return UsernameLink(
+      username,
+    );
+  }
+
+  /// Creates a SettingsLink
+  static SettingsLinks settingsLink() {
+    return SettingsLinks._();
+  }
+
+  /// Creates a [ShareLink] with the given [url] and [text].
+  static ShareLink shareLink(
+    String url, {
+    String? text,
+  }) {
+    return ShareLink(
+      url,
+      text: text,
+    );
+  }
+
+  /// Creates a [MTProxyLink] with the given [server], [port] and [secret].
+  static MTProxyLink mtProxyLink({
+    required String server,
+    required int port,
+    required String secret,
+  }) {
+    return MTProxyLink(
+      server: server,
+      port: port,
+      secret: secret,
+    );
+  }
+
+  /// Creates a [Socks5Link] with the given [server], [port], [username] and [password].
+  static Socks5Link socks5Link({
+    required String server,
+    required int port,
+    String? username,
+    String? password,
+  }) {
+    return Socks5Link(
+      server: server,
+      port: port,
+      username: username,
+      password: password,
+    );
+  }
 }
