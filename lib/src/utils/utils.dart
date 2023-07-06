28d70f3489fac83a691d635b91cfcf7996a9a795
devsdocs <73208025+devsdocs@users.noreply.github.com>
2023-07-06 13:51:28 +0900

    Typesafe for args

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -5,3 +5,9 @@ extension Filename on io.File {
   /// The filename of this file.
   String get filename => path.split('/').last;
 }
+
+/// [String] extension, to clean the [String]
+extension CleanString on String {
+  /// Typesafe for args, replace all white space or multiple space in [String] into single space
+  String get clean => trim().replaceAll(RegExp(r'\s{2,}|[\t\r\n]'), ' ');
+}
