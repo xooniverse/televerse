aa223d092331a5761cf31f3db1131651a77fe1b4
Sreelal TS <heysreelal@gmail.com>
2024-04-15 20:31:30 +0530

    🕵🏻 Custom Predicate Check

--- lib/src/televerse/models/scope_options.dart
+++ lib/src/televerse/models/scope_options.dart
@@ -16,6 +16,25 @@ class ScopeOptions {
   /// and executed if it satisfies the predicate.
   final bool forked;
 
+  /// Custom Predicate Method
+  ///
+  /// Often some handlers require restricted access, like admin controls or bans.
+  /// Checking permissions for each command can be tedious. This is where custom
+  /// predicates come in.
+  ///
+  /// By providing a custom predicate, you can control when an update is processed.
+  /// The update will only be applied if the predicate evaluates to `true`.
+  /// Otherwise, the Bot moves on to the next available handler.
+  ///
+  /// This approach keeps your code clean and avoids repetitive permission checks.
+  ///
+  /// Note: [forked] overrides this. That is even if [customPredicate] may evaluate
+  /// to `false` if [forked] is true the handler is executed.
+  ///
+  /// If by any reason, this method threw any exception, it'll be caught in the passed `Bot.onError` if set.
+  /// Otherwise, treated as `false` evaluation and skips the current handler.
+  final FutureOr<bool> Function(Context ctx)? customPredicate;
+
   /// Constructs a `ScopeOption`.
   ///
   /// - [name] - Name of the Handler Scope. This can be used to remove the scope later.
@@ -24,10 +43,15 @@ class ScopeOptions {
   const ScopeOptions({
     this.forked = false,
     this.name,
+    this.customPredicate,
   });
 
   /// Constructs a `ScopeOption` that is forked. This scope will allow the processing of subsequent handler scope as well.
   ///
   /// - [name] - Name of the Handler Scope. This can be used to remove the scope later.
-  const ScopeOptions.forked({this.name}) : forked = true;
+  /// - [customPredicate] - Middleware to check if the update should be processed or skipped.
+  const ScopeOptions.forked({
+    this.name,
+    this.customPredicate,
+  }) : forked = true;
 }
