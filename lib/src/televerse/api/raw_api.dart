018e69bf7f717356ca31069d7b7deb2d17ff7c5c
Sreelal TS <heysreelal@gmail.com>
2024-07-31 22:13:42 +0530

    🛥️ Bot API 7.8 Changes

--- lib/src/televerse/api/raw_api.dart
+++ lib/src/televerse/api/raw_api.dart
@@ -1945,11 +1945,13 @@ class RawAPI {
     ID chatId,
     int messageId, {
     bool? disableNotification,
+    String? businessConnectionId,
   }) async {
     final params = {
       "chat_id": chatId.id,
       "message_id": messageId,
       "disable_notification": disableNotification,
+      "business_connection_id": businessConnectionId,
     };
     final response = await _makeApiBoolCall(
       APIMethod.pinChatMessage,
@@ -1961,11 +1963,13 @@ class RawAPI {
   /// Use this method to remove a message from the list of pinned messages in a chat. If the chat is not a private chat, the bot must be an administrator in the chat for this to work and must have the 'can_pin_messages' administrator right in a supergroup or 'can_edit_messages' administrator right in a channel. Returns True on success.
   Future<bool> unpinChatMessage(
     ID chatId,
-    int messageId,
-  ) async {
+    int messageId, {
+    String? businessConnectionId,
+  }) async {
     final params = {
       "chat_id": chatId.id,
       "message_id": messageId,
+      "business_connection_id": businessConnectionId,
     };
     final response = await _makeApiBoolCall(
       APIMethod.unpinChatMessage,
