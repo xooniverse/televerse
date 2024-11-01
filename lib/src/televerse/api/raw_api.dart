09411125f348474ba7f8411907049c925301a533
Sreelal TS <heysreelal@gmail.com>
2024-11-01 11:36:38 +0530

    ✍️ Doc update

--- lib/src/televerse/api/raw_api.dart
+++ lib/src/televerse/api/raw_api.dart
@@ -2727,7 +2727,7 @@ class RawAPI {
     return response;
   }
 
-  /// Use this method to edit animation, audio, document, photo, or video messages. If a message is part of a message album, then it can be edited only to an audio for audio albums, only to a document for document albums and to a photo or a video otherwise. When an inline message is edited, a new file can't be uploaded; use a previously uploaded file via its file_id or specify a URL. On success, if the edited message is not an inline message, the edited Message is returned, otherwise True is returned.
+  /// Use this method to edit animation, audio, document, photo, or video messages, or to add media to text messages. If a message is part of a message album, then it can be edited only to an audio for audio albums, only to a document for document albums and to a photo or a video otherwise. When an inline message is edited, a new file can't be uploaded; use a previously uploaded file via its file_id or specify a URL. On success, if the edited message is not an inline message, the edited [Message] is returned.
   ///
   /// IMPORTANT:
   /// * This method is only for editing messages. This won't work for inline messages.
