18d03ad9d31c1c4c57f8a4cde756aaee12ef0b38
Sreelal TS <heysreelal@gmail.com>
2025-01-04 12:55:15 +0530

    ✨ Add `upgradeStarCount` field to Gift model for unique gift upgrades

--- lib/src/telegram/models/gift.dart
+++ lib/src/telegram/models/gift.dart
@@ -17,6 +17,9 @@ class Gift {
   /// Optional. The number of remaining gifts of this type that can be sent; for limited gifts only.
   final int? remainingCount;
 
+  /// Optional. The number of Telegram Stars that must be paid to upgrade the gift to a unique one.
+  final int? upgradeStarCount;
+
   /// Creates a [Gift] object.
   const Gift({
     required this.id,
@@ -24,6 +27,7 @@ class Gift {
     required this.starCount,
     this.totalCount,
     this.remainingCount,
+    this.upgradeStarCount,
   });
 
   /// Creates a [Gift] object from a JSON map.
@@ -34,6 +38,7 @@ class Gift {
       starCount: json['star_count'],
       totalCount: json['total_count'],
       remainingCount: json['remaining_count'],
+      upgradeStarCount: json['upgrade_star_count'],
     );
   }
 
@@ -45,6 +50,7 @@ class Gift {
       'star_count': starCount,
       'total_count': totalCount,
       'remaining_count': remainingCount,
-    };
+      'upgrade_star_count': upgradeStarCount,
+    }..removeWhere(_nullFilter);
   }
 }
